cc132c5b620c4096ec74df003afe39d3c07e663a
Alexander Nazarov <asn.nazarov.asn@gmail.com>
2020-08-26 20:34:14 +0300

    Updates

---
+++ precache-manifest.5c5fa0ebf51e083b3b74cba60c0abd43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776b84366205c0c1ec38bfd69a528972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2c2f18e7664d1f67406",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "68fa3330a41d34aeff67",
+    "url": "/static/js/2.8a5fcf83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a5fcf83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2c2f18e7664d1f67406",
+    "url": "/static/js/main.6ab0612e.chunk.js"
+  },
+  {
+    "revision": "43db3d7fd5a4bcc5a76b",
+    "url": "/static/js/runtime-main.f1618249.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
