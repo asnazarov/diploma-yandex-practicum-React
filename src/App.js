ba6755f8c22912647a899e80e33dcf8b790c7442
Alexander Nazarov <asn.nazarov.asn@gmail.com>
2020-08-26 20:22:07 +0300

    'createAPP'

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, {Component} from "react";
-import {hot} from "react-hot-loader";
-import s from "./app.module.scss";
-import Header from "./Components/Header/Header.jsx";
-import Cover from "./Components/Cover/Cover.jsx";
-import cover from './images/cover.jpg';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className={s.root__item}>
-          <Header/>
-          <Cover />
-        </div>
-
-        <img className={s.main__img} src={cover} alt="cover"/>
-        <h1 className={s.main__title}> 123! </h1>
-        <p>Тут будут новости</p>
-        <p>text</p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default hot(module)(App);
-// export  default App;
+export default App;
